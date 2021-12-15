d293334f698a250856b0b0f85e0ede9a734f0741
kpirogov <pirogovkostya@gmail.com>
2021-12-16 01:50:52 +0300

    add task find coins

--- lesson_string_and_coins_massive/src/Main.java
+++ lesson_string_and_coins_massive/src/Main.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         convert_string();
+        find_coins();
     }
 
     public static void convert_string() {
@@ -37,6 +39,64 @@ public class Main {
             if (current.charValue() != (char) 1 + '0') {
                 System.out.print(current);
             }
+        }
+    }
+
+    public static void find_coins() {
+        //вводим массив монет int
+        Scanner console_scan = new Scanner(System.in);
+        System.out.println("Введите количество монет: ");
+        int N = console_scan.nextInt();
+        int[] coin_array = new int[N];
+        for (int i = 0; i < coin_array.length; i++) {
+            System.out.println("Введите очередной номинал монеты: ");
+            coin_array[i] = console_scan.nextInt();
+        }
+
+        System.out.println("Введите сумму, которую необходимо собрать: ");
+        int summ = console_scan.nextInt();
+
+        //сортируем массив по возрастанию
+        int bubble_proxy = 0;
+        for (int j = 0; j < coin_array.length - 1; j++) {
+            for (int i = j + 1; i < coin_array.length; i++) {
+                if (coin_array[j] > coin_array[i]) {
+                    bubble_proxy = coin_array[j];
+                    coin_array[j] = coin_array[i];
+                    coin_array[i] = bubble_proxy;
+                }
+            }
+        }
+        System.out.println("Имеем монеты с номиналами: ");
+        for (int j : coin_array) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+        System.out.println("Сумма = " + summ);
+
+        //находим номиналы которые нам нужны по принципу "продавщицы" - берем максимально возможные номиналы
+        ArrayList<Integer> result_list = new ArrayList<>();
+        for (int k = coin_array.length - 1; k >= 0; k--) {
+            int number_of_coins = summ / coin_array[k];
+            if (number_of_coins != 0) {
+                result_list.add(coin_array[k]);
+                result_list.add(number_of_coins);
+            }
+            summ = summ - (coin_array[k] * number_of_coins);
+            if (summ <= 0) {
+                break;
+            }
+        }
+
+        System.out.println("Необходимо монет: ");
+        for (int i = 0; i < result_list.size(); i = i + 2) {
+            int current_nominal = result_list.get(i);
+            int current_number = result_list.get(i + 1);
+            System.out.println(current_nominal + "р " + current_number + "шт");
+        }
+        if (summ > 0) {
+            System.out.println("Недостаточно монет данных номиналов, чтобы собрать введенную сумму");
+            System.out.println("Не смогли собрать " + summ + "р");
         }
     }
 }
